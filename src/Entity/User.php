552970c328f973e79092d510007817f2afb5e45c
Piou-piou <pilloud.anthony@gmail.com>
2024-03-27 18:36:37 +0100

    fix: missing orm entity attribute

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+#[ORM\Entity]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
