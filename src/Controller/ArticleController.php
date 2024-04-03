d0fff1e28be6ca72f6fa95c1d40846961e6feb5b
Piou-piou <pilloud.anthony@gmail.com>
2024-04-03 18:54:12 +0200

    feat: add crud for article

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/articles', name: 'articles_')]
+class ArticleController extends AbstractController
+{
+    #[Route('/', name: 'list')]
+    public function list(ArticleRepository $articleRepository): Response
+    {
+        return $this->render('article/list.html.twig', [
+            'articles' => $articleRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/show/{id}', name: 'show')]
+    public function show(Article $article = null)
+    {
+        return $this->render('article/show.html.twig', [
+            'article' => $article
+        ]);
+    }
+
+    #[Route('/edit/{id}', name: 'edit')]
+    #[Route('/create', name: 'create')]
+    public function edit(Request $request, EntityManagerInterface $em, ?Article $article = null): Response
+    {
+        $isCreate = false;
+        if (!$article) {
+            $isCreate = true;
+            $article = new Article();
+        }
+
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var Article $article */
+            $article = $form->getData();
+            $article->setStatus('DRAFT');
+
+            $em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success', $isCreate ? 'L\'article a été créé' : 'L\'article a été modifié');
+
+            return $this->redirectToRoute('articles_list');
+        }
+
+        return $this->render('article/edit.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/delete/{id}', name: 'delete')]
+    public function delete(EntityManagerInterface $em, Article $article): RedirectResponse
+    {
+        $em->remove($article);
+        $em->flush();
+
+        $this->addFlash('success', 'L\'article a été supprimé');
+
+        return $this->redirectToRoute('articles_list');
+    }
+}
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,14 +4,15 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(): RedirectResponse
     {
-        return $this->render('page/index.html.twig');
+        return $this->redirectToRoute('articles_list');
     }
 }
